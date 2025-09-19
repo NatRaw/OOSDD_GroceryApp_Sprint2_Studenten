3ea6854eaea14419b15bc9516ea2fafe6aaebed3
Natan <natan@feest.nl>
2025-09-19 23:25:43 +0200

    UC06 Inloggen: uitgewerkte login functionaliteit - Client class gemaakt voor properties - ClientRepository aangepast aan juiste client en wordt teruggegeven - AuthService uitgebreid met wachtwoordcontrole - LoginView.xaml werkend gekregen - Registraties in MauiProgram.cs geactiveerd - App.xaml.cs aangepast zodat LoginView startpagina is - Route naar LoginView in AppShell geactiveerd

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -5,6 +5,25 @@ namespace Grocery.Core.Models
     {
         private string _emailAddress;
         private string _password { get; set; }
+        
+        // Publieke properties (toegankelijk buiten deze class)
+        public int Id {get; set; }
+        public string Name { get; set; }
+
+        // Property voor het e-mailadres
+        public string EmailAddress
+        {
+            get => _emailAddress;
+            set => _emailAddress = value;
+        }
+
+        //Property voor het wachtwoord
+        public string Password
+        {
+            get => _password;
+            set => _password = value;
+        }
+
         public Client(int id, string name, string emailAddress, string password) : base(id, name)
         {
             _emailAddress=emailAddress;
